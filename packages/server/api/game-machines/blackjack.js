43e230728ed7cb0d72d1788e2639d3d218ceaa3a
Sivan Mehta <Sivanmehta2@gmail.com>
2024-09-06 16:28:10 -0700

    use api to place cards

--- packages/server/api/game-machines/blackjack.js
+++ packages/server/api/game-machines/blackjack.js
@@ -2,6 +2,16 @@ import fetch from 'node-fetch';
 
 const API = 'https://www.deckofcardsapi.com/api/';
 
+async function draw(id, count) {
+  const res = await fetch(
+    `${API}/deck/${id}/draw/?count=${count}`
+  );
+  const { cards } = await res.json();
+  const values = cards.map(card => card.code);
+  return values;
+}
+
+
 export default class Blackjack {
   constructor() {
     this.hand = [];
@@ -9,14 +19,14 @@ export default class Blackjack {
   }
   
   async start() {
-    const res = await fetch(API + 'deck/new/shuffle/?deck_count=6');
-    const data = await res.json();
+    const newDeck = await fetch(API + 'deck/new/shuffle/?deck_count=6');
+    const data = await newDeck.json();
   
     this.id = data.deck_id;
-  }
-
-  async deal(cards) {
-
+    const cards = await draw(this.id, 3);
+    console.log(cards);
+    this.hand.push(cards[0], cards[1]);
+    this.dealer.push(cards[2]);
   }
 }
 
@@ -25,5 +35,5 @@ export function hit(req, res) {
 }
 
 export function stay(req, res) {
-  
+
 }
